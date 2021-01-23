3497b3a24a29570dab2bd6a067b34fbcea645529
Corey Miller <CoreyMiller454@gmail.com>
2021-01-22 23:56:50 -0500

    add query parameter searching

--- routes/card-route.js
+++ routes/card-route.js
@@ -2,24 +2,21 @@ const express = require("express");
 const router = express.Router();
 const cards = require("../models/card");
 
+// router.get("/", async (req, res) => {
+//   try {
+//     console.log("connected to site");
+//     const data = await cards.find({
+//       name: { $regex: "llanowar", $options: "i" },
+//     });
+//     res.send(data);
+//   } catch (err) {
+//     res.send(err);
+//   }
+// });
+
 router.get("/", async (req, res) => {
   try {
-    console.log("connected to site");
-    const data = await cards.find({
-      name: { $regex: "llanowar", $options: "i" },
-    });
-    res.send(data);
-  } catch (err) {
-    res.send(err);
-  }
-});
-
-router.get("/:name", async (req, res) => {
-  try {
-    console.log("connected to site");
-    const data = await cards.find({
-      name: { $regex: req.params.name, $options: "i" },
-    });
+    const data = await cards.find({ name: "elvish" });
     res.send(data);
   } catch (err) {
     res.send(err);
